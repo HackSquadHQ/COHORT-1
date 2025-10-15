6ccb1c373d5e94657f2fb7b3eb7e89385b1e4908
Antia Bukola <bukkyoguns09@gmail.com>
2025-10-15 16:21:40 +0100

    Update bash_Script.sh

--- Week3_Linux for Cybersecurity/bunkunolami/bash_Script.sh
+++ Week3_Linux for Cybersecurity/bunkunolami/bash_Script.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+echo "Hello world"
+chmod + X "Hello World"
+echo " file $hello world" made executable
+This Script demonstrates basic automation using conditional statements ( if- else)
